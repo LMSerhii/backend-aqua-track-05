6daa2835348ba78bda73292c3b93571452b2bec2
Dtimewarp <dtimewarp@gmail.com>
2024-05-09 23:36:48 +0300

    getAllWaterRecordsByDate - done

--- controllers/waterControllers.js
+++ controllers/waterControllers.js
@@ -1,5 +1,8 @@
 import { addWaterAmountService } from "../services/waterServices.js";
 
+import { Water } from "../models/waterModel.js";
+import { User } from "../models/userModel.js";
+
 export const addWaterAmountController = async (req, res) => {
   try {
     const result = await addWaterAmountService(req.user._id, req.body);
@@ -7,5 +10,28 @@ export const addWaterAmountController = async (req, res) => {
   } catch (error) {
     console.error(error);
     return res.status(500).json({ success: false, error: "Помилка сервера" });
+  }
+};
+//
+
+export const getAllWaterRecordsByDate = async (req, res) => {
+  const { id: ownerId } = req.params;
+  const { date } = req.body;
+
+  try {
+    const query = { owner: ownerId };
+
+    if (date) {
+      query.date = date;
+    }
+
+    const waterRecords = await Water.find(query);
+
+    const amounts = waterRecords.map((record) => record.amounts).flat();
+
+    return res.status(200).json({ success: true, data: amounts });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ success: false, error: "Server Error" });
   }
 };
--- routes/waterRouter.js
+++ routes/waterRouter.js
@@ -1,10 +1,14 @@
-import express from 'express';
+import express from "express";
 
-import { addWaterAmountController } from '../controllers/waterControllers.js';
-import { auth } from '../middlewares/authMiddlewares.js';
+import {
+  addWaterAmountController,
+  getAllWaterRecordsByDate,
+} from "../controllers/waterControllers.js";
+import { auth } from "../middlewares/authMiddlewares.js";
 
 const waterRouter = express.Router();
 
-waterRouter.post('/add', auth, addWaterAmountController);
+waterRouter.post("/add", auth, addWaterAmountController);
+waterRouter.get("/:id", auth, getAllWaterRecordsByDate);
 
 export default waterRouter;
