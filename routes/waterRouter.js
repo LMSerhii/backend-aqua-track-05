92b0e3fecd99af9c6b28f15343f1c347497c550c
Dtimewarp <dtimewarp@gmail.com>
2024-05-13 00:28:26 +0300

    add daily/edit/detele/month endpoints

--- routes/waterRouter.js
+++ routes/waterRouter.js
@@ -1,14 +1,20 @@
 import express from "express";
 
 import {
-  addWaterAmountController,
+  addWaterAmount,
   countAllWaterRecordsByDate,
+  getWaterRecordsByCurrentUserAndMonth,
+  removeWaterAmount,
+  updateWaterAmount,
 } from "../controllers/waterControllers.js";
 import { auth } from "../middlewares/authMiddlewares.js";
 
 const waterRouter = express.Router();
 
-waterRouter.post("/add", auth, addWaterAmountController);
-waterRouter.get("/:id", auth, countAllWaterRecordsByDate);
+waterRouter.post("/add", auth, addWaterAmount);
+waterRouter.get("/daily_count", auth, countAllWaterRecordsByDate);
+waterRouter.put("/edit/:id", auth, updateWaterAmount);
+waterRouter.patch("/delete/:id", auth, removeWaterAmount);
+waterRouter.post("/month", auth, getWaterRecordsByCurrentUserAndMonth);
 
 export default waterRouter;
--- services/waterServices.js
+++ services/waterServices.js
@@ -1,3 +1,5 @@
+import mongoose from "mongoose";
+
 import { Water } from "../models/waterModel.js";
 
 export const addWaterAmountService = async (ownerID, body) => {
@@ -21,5 +23,108 @@ export const addWaterAmountService = async (ownerID, body) => {
   } catch (error) {
     console.error(error);
     return { success: false, statusCode: 500, error: "Помилка сервера" };
+  }
+};
+
+export const countTotalAmountByDateService = async (ownerId, date) => {
+  try {
+    const query = { owner: ownerId };
+
+    if (date) {
+      query.date = date;
+    }
+
+    const waterRecords = await Water.find(query);
+
+    const amounts = waterRecords.map((record) => record.amounts).flat();
+
+    const totalAmount = amounts.reduce((sum, record) => sum + record.amount, 0);
+
+    return { date, data: amounts, totalAmount };
+  } catch (error) {
+    console.error(error);
+    throw new Error("Server Error");
+  }
+};
+
+export const updateWaterAmountByIdService = async (
+  recordId,
+  amountId,
+  amount,
+  time
+) => {
+  try {
+    const waterRecord = await Water.findById(recordId);
+
+    if (!waterRecord) {
+      throw new Error("Record not found");
+    }
+
+    const mongooseId = mongoose.Types.ObjectId.isValid(amountId)
+      ? amountId
+      : mongoose.Types.ObjectId(amountId);
+
+    const updatedAmount = waterRecord.amounts.id(mongooseId);
+
+    if (!updatedAmount) {
+      throw new Error("Amount not found");
+    }
+
+    updatedAmount.amount = amount;
+    updatedAmount.time = time;
+
+    await waterRecord.save();
+
+    return updatedAmount;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+export const deleteWaterAmountService = async (recordId, amountId) => {
+  try {
+    const waterRecord = await Water.findById(recordId);
+
+    if (!waterRecord) {
+      return { success: false, statusCode: 404, error: "Record not found" };
+    }
+
+    const index = waterRecord.amounts.findIndex(
+      (amount) => amount._id.toString() === amountId
+    );
+    if (index === -1) {
+      return { success: false, statusCode: 404, error: "Amount not found" };
+    }
+
+    waterRecord.amounts.splice(index, 1);
+
+    await waterRecord.save();
+
+    return { success: true };
+  } catch (error) {
+    console.error(error);
+    return { success: false, statusCode: 500, error: "Server Error" };
+  }
+};
+
+export const getWaterRecordsByUserAndMonth = async (userId, month) => {
+  try {
+    let waterRecords = await Water.find({ owner: userId });
+
+    if (month) {
+      const [mm, yyyy] = month.split("-");
+      const startDate = new Date(parseInt(yyyy, 10), parseInt(mm, 10) - 1, 1);
+      const endDate = new Date(parseInt(yyyy, 10), parseInt(mm, 10), 0);
+
+      waterRecords = waterRecords.filter((record) => {
+        const recordDate = new Date(record.date.split("-").reverse().join("-"));
+
+        return recordDate >= startDate && recordDate <= endDate;
+      });
+    }
+
+    return waterRecords;
+  } catch (error) {
+    throw new Error("Server Error");
   }
 };
