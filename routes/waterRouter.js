7a387f287f6fa46b0cc348081fadb46703de4870
LMSerhii <117682588+LMSerhii@users.noreply.github.com>
2024-05-14 19:04:32 +0300

    Update waterRouter.js

--- routes/waterRouter.js
+++ routes/waterRouter.js
@@ -12,7 +12,7 @@ import { auth } from "../middlewares/authMiddlewares.js";
 const waterRouter = express.Router();
 
 waterRouter.post("/add", auth, addWaterAmount);
-waterRouter.get("/daily_count", auth, countAllWaterRecordsByDate);
+waterRouter.post("/daily_count", auth, countAllWaterRecordsByDate);
 waterRouter.put("/edit/:id", auth, updateWaterAmount);
 waterRouter.patch("/delete/:id", auth, removeWaterAmount);
 waterRouter.post("/month", auth, getWaterRecordsByCurrentUserAndMonth);
